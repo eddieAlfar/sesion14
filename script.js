c3d991d1e15944440e6aaf7037732c0157bc9c0c
eddieAlfaro <edbie2415@hotmail.com>
2022-11-15 17:15:02 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,46 @@
+//Ejercicio n°1
+let hombre = +prompt("Ingresa cant hombres")
+let mujeres = +prompt("Ingresa cant mujeres")
+let total = (hombre+mujeres)
+
+alert(`Cantidad de hombres ${(hombre/total)*100}`)
+alert(`Cantidad de mujeres ${(mujeres/total)*100}`)
+
+/*
+//Ejercicio n°2
+let monto = +prompt("Ingresa monto")
+document.write (`Av. La Mar recibe ${(monto*35)/100}`)
+document.write(`<br>`)
+document.write (`Av. Abancay recibe ${(monto*25)/100}`)
+document.write(`<br>`)
+document.write (`Av. 28 de Julio recibe ${(monto*10)/100}`)
+document.write(`<br>`)
+document.write (`Avenida Aviacion recibe ${(monto*15)/100}`)
+document.write(`<br>`)
+document.write (`Av Tacna recibe ${monto*15/100}`)
+document.write(`<br>`)
+*/
+//Ejercicio n°3
+/*
+let entradagen=+prompt("Ingresa monto")
+let entradamay=+prompt("Ingresa monto")
+let entradamen=+prompt("Ingresa monto")
+let precioGen = 150
+let precioMay = 50
+let precioMen = 80
+const total=(a,b)=>{
+    return a*b
+}
+let recaudadofinal=
+    total(entradagen,precioGen)+
+    total(entradamay,precioMay)+
+    total(entradamen,precioMen)
+document.write (`El monto de la entrada general es S/. ${entradagen*150}`)
+document.write(`<br>`)
+document.write (`El monto de la entrada Mayores es S/. ${entradamay*50}`)
+document.write(`<br>`)
+document.write (`El monto de la entrada menores es S/. ${entradamen*80}`)
+document.write(`<br>`)
+document.write (`El monto de la entrada menores es S/.`+recaudadofinal )
+document.write(`<br>`)
+*/
